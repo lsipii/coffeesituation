16ad9f042aed21f06b48f7ee1a1ba9a4f3570888
Lassi Piironen <lsipii@kapsi.fi>
2018-02-26 02:19:37 +0200

    rediet the image stuff

--- features/CoffeeChecker.py
+++ features/CoffeeChecker.py
@@ -3,6 +3,8 @@
 @author lsipii
 """
 import sh
+import base64 
+import datetime
 
 class CoffeeChecker():
 
@@ -10,7 +12,10 @@ class CoffeeChecker():
 	Module initialization
 	"""
 	def __init__(self):
+		self.photoShootTime = None
 		self.imageDirectory = "~/.zoinks/coffee"
+		self.imageFilename = "cameraOutput.jpg"
+		self.imagePath = self.imageDirectory+"/"+self.imageFilename
 		sh.mkdir("-p", self.imageDirectory)
 
 	"""
@@ -19,15 +24,48 @@ class CoffeeChecker():
 	@return (bool) weHave
 	"""
 	def hasWeCoffee(self):
-		return {"message": "maybe?"}
+
+		if self.shouldWeTakeAPhoto(): 
+			self.takeACoffeePhoto(self.imagePath)
+		imageBinStr = self.readImageAsB64String(self.imagePath)
+
+		return {"message": "maybe?", "image": imageBinStr}
+
+	
+	
+	"""
+	Checks if we should take a photo indeed
+	
+	@return (bool) 
+	"""
+	def shouldWeTakeAPhoto(self, imagePath):
+		if self.photoShootTime is None
+			return True
+
+		howLongAgoLastShoot = datetime.now() - self.photoShootTime
+		if howLongAgoLastShoot.total_seconds() > 30:
+			return True
+			
+		return False
+		
 
 	"""
 	Takes a photo
-
-	@return (string) imagePath
+	
+	@param (string) imagePath
 	"""
-	def takeACoffeePhoto(self):
-		imageDirectory
-		imagePath=self.imageDirectory+"/cameraOutput.jpg"
+	def takeACoffeePhoto(self, imagePath):
+		self.photoShootTime = datetime.now()
 		sh.raspistil('-o', imagePath)
-		return imagePath
+
+	"""
+	Reads taken photo as base64 bin string
+	
+	@param (string) imagePath
+	@return (base64 string) image64
+	"""
+	def readImageAsB64String(self, imagePath):
+		image = open(imagePath, 'rb') #open binary file in read mode
+		imageRead = image.read()
+		image64 = base64.encodestring(imageRead)
+		return image64
