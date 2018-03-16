67b5ac369be0944dcabe0f23457b5202a0b50670
Lassi Piironen <lsipii@kapsi.fi>
2018-03-16 23:11:09 +0200

    ... prev

---
+++ deviceApp.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+@author lsipii
+"""
+import sys, getopt
+from flask import Flask
+from device.DeviceApp import DeviceApp
+from device.http.controllers.CoffeesHasWeController import CoffeesHasWeController
+
+# Creates the device app
+app = DeviceApp()
+# Creates the flask router app
+routerApp = Flask(__name__)
+# The app controller
+controller = CoffeesHasWeController(app)
+
+# Defines the app routes
+@routerApp.route('/', methods=controller.knownHttpMethods)
+@routerApp.route('/<path>', methods=controller.knownHttpMethods)
+def request(path = None):
+	return controller.getCoffeeResponse(path)
+
+# App runner
+if __name__ == '__main__':
+
+	argv = sys.argv[1:]
+	
+	# Sets the app debug mode
+	debugMode = True
+
+	# Help texts
+	def printHelp():
+		print(app.getFullAppName())
+		print("Mission statement: "+ app.getAppMissionStatement(), "\n")
+		print("Usage: zoinks.py --help|--version|--production") 
+		exit()
+		
+	try:
+		opts, args = getopt.getopt(argv, "hvp", ["help", "version", "production"])
+	except getopt.GetoptError:
+		printHelp()
+
+	for opt, arg in opts:
+
+		if opt in ("-h", "--help"):
+			printHelp()
+		if opt in ("-v", "--version"):
+			print(app.getFullAppName())
+			exit()
+		if opt in ("-p", "--production"):
+			debugMode = False
+
+	# Run app
+	controller.setDebugMode(debugMode)
+	controller.validateZoinksFunctionality()
+	routerApp.run(debug=debugMode)
---
+++ slackbot/SlackBotApp.py
@@ -0,0 +1,31 @@
+"""
+@author lsipii
+"""
+
+from app.AppInfo import AppInfo
+from app.ConfigReader import ConfigReader
+
+"""App details container"""
+class SlackBotApp(AppInfo):
+
+	"""
+	App initialization
+	"""
+	def __init__(self):
+		self.config = ConfigReader("slackbot").getConfig() 
+
+	"""
+	Gets the app name
+
+	@return (string)
+	"""
+	def getAppName(self):
+		return "Coffee Related Communication And Relations Facilitator"
+
+	"""
+	Gets the app mission statement
+
+	@return (string)
+	"""
+	def getAppMissionStatement(self):
+		return "Facilitate communication between species and put an end to hostilities"
