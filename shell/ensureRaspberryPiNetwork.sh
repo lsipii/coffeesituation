e93003d0e95c32cba0bb223ea5c44a8e46fe7b5f
Lassi Piironen <lsipii@kapsi.fi>
2018-04-17 10:41:09 +0300

    Switched the network ensurer back to rebootlessnes

--- shell/ensureRaspberryPiNetwork.sh
+++ shell/ensureRaspberryPiNetwork.sh
@@ -2,11 +2,14 @@
 # @author lsipii
 #  
 # Example crontask, once in hour:
-# 10 * * * * ensureRaspberryPiNetwork.sh
+# */20 * * * * ensureRaspberryPiNetwork.sh
 
 # Ping google, restart interfaces on a connection error
 ping -c2 8.8.8.8 > /dev/null
 if [ $? != 0 ]; then
-	echo "Restarting network connection.. by rebooting!"
-	sudo reboot
+	echo "Restarting network connection.."
+	sudo ifdown --force wlan0
+	sudo ifup wlan0
+	sleep 10
+	sudo ifup wlan0
 fi
